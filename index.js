a1c41f9f11cbdd73986c79862d57322a18da9001
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2018-09-11 15:44:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Setting ejs as view engine
+app.set("view engine", "ejs");
+
+// Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    "mongodb://mongo:27017/docker-node-mongo",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+const Item = require("./models/Item");
+
+app.get("/", (req, res) => {
+  Item.find()
+    .then(items => res.render("index", { items }))
+    .catch(err => res.status(404).json({ msg: "No items found" }));
+});
+
+app.post("/item/add", (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save().then(item => res.redirect("/"));
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
